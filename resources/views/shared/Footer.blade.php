095865a8e0ed18988e9e3c5880f6325574df6837
Yakumo-Sahashi <Diego.Bollas@hotmail.com>
2023-11-18 01:20:30 -0600

    visual

--- resources/views/shared/Footer.blade.php
+++ resources/views/shared/Footer.blade.php
@@ -5,13 +5,13 @@
             <div class="order-2 order-md-1 col-md-5 text-center text-md-end align-self-center">
                 <p class="lead TamDeLetras mb-0"><b><i class="fa-solid fa-graduation-cap me-1"></i>Plantel</b></p>
                 <p class="lead TamDeLetras">Cbt N° 3 Sr. Max Shein Heisler Chalco</p>
-                <p class="lead TamDeLetras"><i class="fa-solid fa-phone me-2"></i>Tel: 55-17-09-66-17</p>
+                <p class="lead TamDeLetras"><b><i class="fa-solid fa-phone me-2"></i>Tel: 55-17-09-66-17</b></p>
             </div>
             <div class="order-1 order-md-2 col-md-2 align-self-center">
                 <img src="{{ asset('img/logocbt3.webp') }}" class="TamDeImgDeFooter mx-auto d-block mb-3" alt="">
             </div>
             <div class="order-3 order-md-3 col-md-5 text-center text-md-start align-self-center">
-                <p class="lead TamDeLetras mb-0"><i class="fa-solid fa-map-location-dot me-2"></i>Dirección</p>
+                <p class="lead TamDeLetras mb-0"><b><i class="fa-solid fa-map-location-dot me-2"></i>Dirección</b></p>
                 <p class="lead TamDeLetras">Calle Carretera Chalco - Maseca , Santa Catarina Ayotzingo Chalco. CP. 56623</p>
             </div>
         </div>
